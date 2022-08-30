771d117dcd6f74c1293c0e097453b223973745cc
Beniko19 <martin192012@gmail.com>
2022-08-30 16:50:17 -0300

    added the implementation to set the fee, calculate the fix with its validation.

--- src/cards/NaraCreditCard.java
+++ src/cards/NaraCreditCard.java
@@ -1,19 +1,28 @@
 package cards;
 
+import exceptions.AmountExceededException;
+
 public class NaraCreditCard extends CreditCard{
 
     private double fee;
     public NaraCreditCard(String issuer, String cardNumber, String cardHolder, String expirationDate) throws Exception {
         super(issuer, cardNumber, cardHolder, expirationDate);
+        setFee();
     }
 
     @Override
     public String feeOperation(Double money) {
-        return null;
+        if(isOperationValid(money))
+            return "The fee for the operation is: $" + String.format("%.2f",fee*money);
+        else
+            throw new AmountExceededException("The operation can only be carried out with a value of up to $999.99");
     }
 
     @Override
     protected void setFee() {
-
+        String result = super.getExpirationDate().replaceAll("/", "");
+        final int mid = result.length() / 2;
+        String[] parts = {result.substring(0, mid), result.substring(mid)};
+        fee = Double.parseDouble(parts[0]) * 0.1;
     }
 }
