c7d417d1cdfd24774ae370690d15f54ac3c81758
Beniko19 <martin192012@gmail.com>
2022-08-30 14:32:19 -0300

    added an abstract class of a credit card where is called a method from another class to verify if the card information is right.

--- src/cards/CreditCard.java
+++ src/cards/CreditCard.java
@@ -1,11 +1,25 @@
 package cards;
 
-import java.util.regex.Pattern;
-
 public abstract class CreditCard {
 
     private String issuer;
     private String cardNumber;
     private String cardHolder;
-    private Pattern expirationDate;
+    private String expirationDate;
+
+    public CreditCard(String issuer, String cardNumber, String cardHolder, String expirationDate) throws Exception {
+        VerifyCreditCard.validateCreditCardDates(issuer, cardNumber, cardHolder, expirationDate);
+        this.issuer = issuer;
+        this.cardNumber = cardNumber;
+        this.cardHolder = cardHolder;
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return  " issuer='" + issuer + '\'' +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", cardHolder='" + cardHolder + '\'' +
+                ", expirationDate='" + expirationDate;
+    }
 }
