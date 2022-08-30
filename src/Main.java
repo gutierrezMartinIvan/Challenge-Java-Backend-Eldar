705231235b3692ec3f9e50f893c0e1f60d4e0a08
Beniko19 <martin192012@gmail.com>
2022-08-30 17:01:56 -0300

    updated the main with some cases, please edit them as much as possible to test the app.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import cards.AmexCreditCard;
+import cards.CreditCard;
+import cards.NaraCreditCard;
+import cards.VisaCreditCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        List<CreditCard> cards = new ArrayList<>();
+        cards.add(new AmexCreditCard("AMEX", "375987654321001", "GUTIERREZ MARTIN IVAN", "12/25"));
+        cards.add(new VisaCreditCard("VISA", "4464646464657689", "MARTIN IVAN", "05/27"));
+        cards.add(new NaraCreditCard("NARA", "4575869867932312", "IVAN", "09/29"));
+        cards.add(new NaraCreditCard("NARA", "4575869867932312", "IVAN", "09/29"));
+        cards.forEach(card -> {
+            System.out.println(card.getCreditCardInfo());
+            System.out.println(card.isCreditCardUsable());
+        });
+
+        System.out.println(cards.get(0).feeOperation(999D));
+        System.out.println(cards.get(1).feeOperation(999D));
+        System.out.println(cards.get(2).feeOperation(999D));
+
+        System.out.println(cards.get(1).areEquals(cards.get(2)));
+        System.out.println(cards.get(2).areEquals(cards.get(3)));
+
     }
 }
