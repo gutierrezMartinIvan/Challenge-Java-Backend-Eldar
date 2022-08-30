a5e862f5ff1da052fbb4046628972df87f86fcb1
Beniko19 <martin192012@gmail.com>
2022-08-30 14:29:42 -0300

    added the amex credit card.

---
+++ src/cards/AmexCreditCard.java
@@ -0,0 +1,13 @@
+package cards;
+
+public class AmexCreditCard extends CreditCard{
+
+    public AmexCreditCard(String issuer, String cardNumber, String cardHolder, String expirationDate) throws Exception {
+        super(issuer, cardNumber, cardHolder, expirationDate);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() ;
+    }
+}
