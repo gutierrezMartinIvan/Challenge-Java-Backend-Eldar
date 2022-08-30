e447e8d59274a019adf219f1f63d7d5b71fb4655
Beniko19 <martin192012@gmail.com>
2022-08-30 16:28:15 -0300

    Implemented the method to calculate the fee of a operation and verify is the amount is between the right wide/range.

--- src/cards/VisaCreditCard.java
+++ src/cards/VisaCreditCard.java
@@ -1,19 +1,32 @@
 package cards;
 
+import exceptions.AmountExceededException;
+import exceptions.InvalidIssuerException;
+
+import java.util.stream.IntStream;
+
 public class VisaCreditCard extends CreditCard{
+
+    private double fee;
 
     public VisaCreditCard(String issuer, String cardNumber, String cardHolder, String expirationDate) throws Exception {
         super(issuer, cardNumber, cardHolder, expirationDate);
-    }
-
-
-    @Override
-    public String doOperation() {
-        return null;
+        setFee();
     }
 
     @Override
-    public boolean isOperationValid() {
-        return false;
+    public String feeOperation(Double money) {
+        if(isOperationValid(money))
+            return "The fee for the operation is: $" + String.format("%.2f",fee*money);
+        else
+            throw new AmountExceededException("The operation can only be carried out with a value of up to $999.99");
+    }
+
+    @Override
+    protected void setFee() {
+        String result = super.expirationDate.replaceAll("/", "");
+        final int mid = result.length() / 2;
+        String[] parts = {result.substring(0, mid), result.substring(mid)};
+        fee = Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]);
     }
 }
